c9ba5c1a7158148aab47f33bf5d0f844c9cecd04
mohammedayaz24 <mohammedayaz2411@gmail.com>
2026-07-09 11:57:15 +0530

    react overview

---
+++ module 1/app.js
@@ -0,0 +1,9 @@
+
+//default
+let a = 7;
+
+export default a
+
+//named 
+
+export let arr = [10,20,30,40,50]
---
+++ module 1/script.js
@@ -0,0 +1,8 @@
+//default 
+import a from './app.js'
+console.log(a);
+
+
+///named
+import {arr} from './app.js'
+console.log(arr);

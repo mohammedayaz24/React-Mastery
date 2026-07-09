22eae5b3b55263680c57558e3af51b0ff5e61616
mohammedayaz24 <mohammedayaz2411@gmail.com>
2026-07-09 21:16:14 +0530

    props and more

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -3,9 +3,11 @@ const App = () => {
   return (
     <>
     <div className='parent'>
-      <Card/>
-      <Card user='Iron man' role="Ai engineer"/>
-      <Card/>
+      <Card user="Mohammed Ayaz" 
+            role="Software Developer"
+            url="https://images.unsplash.com/photo-1783201033538-85c76cf0ba05?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEyfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D"/>
+      <Card user="Zakwan" role="AI engineer" url="https://images.unsplash.com/photo-1779896412420-d37179cdffb9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8"/>
+      <Card user="Faazil ammar" role="data analyst" url="https://plus.unsplash.com/premium_photo-1783339296675-777f8ed794fc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D"/>
     </div>
     </>
     
--- 03-props/src/components/Card.jsx
+++ 03-props/src/components/Card.jsx
@@ -1,15 +1,17 @@
 
 const Card = (props) => {
+  console.log(props)
   return (
     <>
       <div className="card">
-        <img src="https://images.unsplash.com/photo-1783201033538-85c76cf0ba05?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEyfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D"></img>
-        <h1>Mohammed Ayaz</h1>
-        <p>Software Developer</p>
+        <img src={props.url}></img>
+        <h1>{props.user}</h1>
+        <p>{props.role}</p>
         <button className="btn">View</button>
       </div>
     </>
   )
 }
+
 
 export default Card

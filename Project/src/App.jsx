42415435f42133bc9a5891b881e510f399681f73
mohammedayaz24 <mohammedayaz2411@gmail.com>
2026-07-09 14:47:26 +0530

    react setup

---
+++ Project/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>//
+    <div id="parent">
+      <h1 id='child1'>Hello from child 1</h1>
+      <h1 id='child2'>Hello from child 2</h1>
+    </div>
+    </>
+  )
+}
+
+export default App

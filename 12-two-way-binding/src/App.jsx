9e803e451970248ceed79d9d9aeb7677acafe287
mohammedayaz24 <mohammedayaz2411@gmail.com>
2026-07-19 23:58:30 +0530

    self test in two way binding

--- 12-two-way-binding/src/App.jsx
+++ 12-two-way-binding/src/App.jsx
@@ -3,12 +3,15 @@ import { useState } from "react";
 const App = () => {
 
   const [title,setTitle] = useState('')
+  const [key,setKey] = useState('')
 
   const submitted = (e) => {
     e.preventDefault();
     console.log("Form Submitted by:",title);
+    console.log("The password is:",key)
 
     setTitle('')//to reset the title
+    setKey('')
   }
     
   return (
@@ -19,6 +22,13 @@ const App = () => {
                value={title}
                onChange={(e) => {
                  setTitle(e.target.value);
+               }} />
+
+        <input type="password"
+               placeholder="Enter your Password"
+               value={key}
+               onChange={(e) => {
+                setKey(e.target.value);
                }} />
         <button>Submit</button>
       </form>
